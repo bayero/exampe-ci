383e2de6cc09126c7ea250bab319edd0f2bf3da2
bayero <sambobayero93@gmail.com>
2022-11-03 17:53:12 +0000

    first

---
+++ src/main/java/com/ne/exampleci/ExampleCiApplication.java
@@ -0,0 +1,21 @@
+package com.ne.exampleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleCiApplication.class, args);
+    }
+
+}
+/*
+
+    git init
+    git add README.md
+        git commit-m"first commit"
+        git branch-M main
+        git remote add origin https://github.com/bayero/exampe-ci.git
+        git push-u origin main*/
---
+++ src/test/java/com/ne/exampleci/ExampleCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ne.exampleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
