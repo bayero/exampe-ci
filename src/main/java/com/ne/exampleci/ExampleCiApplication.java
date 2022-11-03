520d065437bbd32ab7722a0f0f6214b4d7c94b39
bayero <sambobayero93@gmail.com>
2022-11-03 17:55:58 +0000

    add endPoind

--- src/main/java/com/ne/exampleci/ExampleCiApplication.java
+++ src/main/java/com/ne/exampleci/ExampleCiApplication.java
@@ -2,20 +2,19 @@ package com.ne.exampleci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ExampleCiApplication {
+
+    public String welcome() {
+        return "Salut sambo bienvenue sur ton test d'integration";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleCiApplication.class, args);
     }
 
 }
-/*
 
-    git init
-    git add README.md
-        git commit-m"first commit"
-        git branch-M main
-        git remote add origin https://github.com/bayero/exampe-ci.git
-        git push-u origin main*/
