2d2e2395126b653f9e763223dd5d61b9470e33f9
bayero <sambobayero93@gmail.com>
2022-11-04 09:46:34 +0000

    add endPoint v1

--- src/main/java/com/ne/exampleci/ExampleCiApplication.java
+++ src/main/java/com/ne/exampleci/ExampleCiApplication.java
@@ -3,13 +3,15 @@ package com.ne.exampleci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping(value = "welcome")
 public class ExampleCiApplication {
 
-    @GetMapping("/")
+    @GetMapping("sam")
     public String welcome() {
         return "Salut sambo bienvenue sur ton test d'integration";
     }
